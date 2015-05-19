231d92fe18119009b219e0fa2c93645961d052b0
NZMia <mia7zwy@hotmail.com>
2015-05-20 11:07:31 +1200

    Initial Commit

---
+++ MZ_ContentList_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MZ_ContentList_test
+//
+//  Created by Mia.Zhang on 20/05/15.
+//  Copyright (c) 2015 Mia.Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MZ_ContentList_testTests/MZ_ContentList_testTests.m
@@ -0,0 +1,40 @@
+//
+//  MZ_ContentList_testTests.m
+//  MZ_ContentList_testTests
+//
+//  Created by Mia.Zhang on 20/05/15.
+//  Copyright (c) 2015 Mia.Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MZ_ContentList_testTests : XCTestCase
+
+@end
+
+@implementation MZ_ContentList_testTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
